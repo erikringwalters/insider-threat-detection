516b83be503efa4f2d7ebbe6e6385d321aa2f947
Josh <joshuwilliams@csumb.edu>
2019-04-17 14:21:42 -0700

    Containers now accept input

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-container',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
+
+  @Input() title: String;
 
   constructor() { }
 
