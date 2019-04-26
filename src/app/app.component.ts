618bc72b1921c84d5fd2e979ff87987bf509610e
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-04-25 17:03:02 -0700

    changed title an favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'insider-threat-detection';
+  title = 'Insider Threat Detection';
 }
